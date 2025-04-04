d39dc9e43d575d588b17741d3f7c5bb521ec570b
Abdur Rehman Bin Masud <abdurrehmanbinmasud@outlook.com>
2025-04-04 14:21:07 +0500

    commit

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db'); // we'll create this file
-const authRoutes = require('./routes/authRoutes'); // we'll create this file
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes"); 
 
 dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 connectDB();
 
-const app = express();
-
-// Middlewares
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 
-// Routes
-app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRoutes); 
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
