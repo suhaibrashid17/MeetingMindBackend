af191be78cfe9a81aea225d477c945e3473873df
Abdur Rehman Bin Masud <abdurrehmanbinmasud@outlook.com>
2025-04-04 13:53:43 +0500

    push

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["organization_admin", "department_admin", "user"],
+    default: "user",
+  },
+  department: {
+    type: String,
+    default: null,
+  },
+});
+
+// Password hash before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
