98e93a81c820f0f2399f2d2cd600112995b95560
Abdur Rehman Bin Masud <abdurrehmanbinmasud@outlook.com>
2025-04-13 14:52:43 +0500

    commit

--- index.js
+++ index.js
@@ -4,24 +4,39 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+//toimport custom files
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes"); 
+const authRoutes = require("./routes/authRoutes");
+const protectedRoutes = require("./routes/protectedRoutes");
+
 
 dotenv.config();
+
+// app setup
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// connect to MongoDB
 connectDB();
 
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true,
+  origin: "http://localhost:3000", // frontend address
+  credentials: true, // allow cookies from frontend
 }));
 
-app.use("/api/auth", authRoutes); 
+// API routes
+app.use("/api/auth", authRoutes);
+app.use("/api/protected", protectedRoutes);
 
+// Test Route
+app.get("/", (req, res) => {
+  res.send("✅ Backend running");
+});
+
+// Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
