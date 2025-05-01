cc76287c8cb0c5bc32660ec9156a06e40933f668
Suhaib Rashid <suhaibrashid2003@gmail.com>
2025-05-01 20:36:26 +0500

    changes

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api", organizationRoutes);
